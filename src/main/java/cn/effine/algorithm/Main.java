9bf6930c82ffd40c921043c2b49b54792dee3c7c
effine <zhang.yafei@foundbyte.com>
2024-06-17 09:53:00 +0800

    初始化

---
+++ src/main/java/cn/effine/algorithm/Main.java
@@ -0,0 +1,13 @@
+package cn.effine.algorithm;
+
+
+/**
+ * @author effine
+ * @email iballad#163.com
+ * @date 2024-06-17 09:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
